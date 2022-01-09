fc187e1c29370be7c16c92109cd2aec10a08a798
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-09 17:47:37 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#Mathematical Operations in Python
+
+print(3+4)
+print(9-3)
+print(3*5)
+print(6/2)
+print(2**3)
+
+
+# Associativity and Precedence
+# PEMDAS(left to the right order)
+# Parantheses     ()
+# Exponent        **
+# Multiplication  *
+# Division        /
+# Addition        +
+# Subtraction     -
+
+#Example
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
